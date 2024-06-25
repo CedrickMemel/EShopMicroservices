2786a44f8d297b0942d6bf288dcc0236372b585b
CedrickMemel <cedrickmemel@gmail.com>
2024-06-25 15:22:16 +0200

    regrouping all the development till the implementation of the getproduct endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductResult(Guid Id);
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>
+    {
+    }
+    public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //TODO
+            //Save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            //return result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
