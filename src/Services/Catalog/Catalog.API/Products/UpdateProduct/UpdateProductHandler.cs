d4188ad597d715ccaac450eb2d7831ce0d357df4
CedrickMemel <cedrickmemel@gmail.com>
2024-06-26 17:06:02 +0200

    Update product Endpoint implemented

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool isUpdatedTrue);
+public class UpdateProductHandler(IDocumentSession session, ILogger<UpdateProductHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+
+        var product = await session.LoadAsync<Product>(command.id, cancellationToken);
+
+        if (product is null) throw new ProductNotFoundException();
+
+        product.Name = command.Name;
+        product.Category = command.Category;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+        product.Description = command.Description;
+
+        session.Update(product);
+        await session.SaveChangesAsync();
+
+        return new UpdateProductResult(true);
+
+    }
+}
